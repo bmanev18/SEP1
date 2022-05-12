796d1fc4e9546037996452a998f5fadb75c47c63
Bozhidar Manev <bojomanev@gmail.com>
2022-05-12 15:41:18 +0200

    RoomList added

---
+++ src/RoomList.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class RoomList {
+    private ArrayList<Room> rooms;
+
+    public RoomList() {
+        rooms = new ArrayList<>();
+    }
+
+    public void addRoom(Room room) {
+        rooms.add(room);
+    }
+
+    public void removeRoom(Room room) {
+        rooms.remove(room);
+    }
+
+    public Room getRoom(int number) {
+        for (int i = 0; i < rooms.size(); i++) {
+            if (rooms.get(i).getRoomNumber() == number) {
+                return rooms.get(i);
+            }
+        }
+        return null;
+    }
+
+}
