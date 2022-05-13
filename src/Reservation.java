b0464351dcbf78160da95caaea0b7be2a2d9648b
joan1821 <gwantamo35@gmail.com>
2022-05-13 19:59:04 +0200

    added total price method
    the last method(total price ) was added

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,74 +1,88 @@
-public class Reservation {
-    private RoomList roomList;
-    private DateInterval dateInterval;
-    private GuestList guestList;
-    private double price;
-    private int numberExpectedGuests;
-    private boolean extraBed;
+public class Reservation
+{
+  private RoomList roomlist;
+  private DateInterval dateInterval;
+  private GuestList guestList;
+  private double price;
+  private int numberExpectedGuests;
+  private boolean extraBed;
 
-
-    public Reservation(GuestList guestList, DateInterval dateInterval, RoomList roomList,
-                       int numberExpectedGuests) {
-        this.guestList = guestList;
-        this.dateInterval = dateInterval;
-        this.roomList = roomList;
-        this.numberExpectedGuests = numberExpectedGuests;
-        for (Room room : this.roomList.getAllRooms()) {
-            this.price += room.getPrice();
-        }
-
+  public Reservation(GuestList guestList, DateInterval dateInterval,
+      RoomList roomlist, int numberExpectedGuests)
+  {
+    this.guestList = guestList;
+    this.dateInterval = dateInterval;
+    this.roomlist = roomlist;
+    this.numberExpectedGuests = numberExpectedGuests;
+    for (Room room : this.roomlist.getAllRooms())
+    {
+      this.price += room.getPrice();
     }
 
-    public RoomList getRoomList() {
-        return roomList;
-    }
+  }
 
-    public void setRoomList(RoomList roomList) {
-        this.roomList = roomList;
-    }
+  public RoomList getRoomList()
+  {
+    return roomlist;
+  }
 
-    public DateInterval getDateInterval() {
-        return dateInterval;
-    }
+  public DateInterval getDateInterval()
+  {
+    return dateInterval;
+  }
 
-    public void setDateInterval(DateInterval dateInterval) {
-        this.dateInterval = dateInterval;
-    }
+  public void setDateInterval(DateInterval dateInterval)
+  {
+    this.dateInterval = dateInterval;
+  }
 
-    public GuestList getGuestList() {
-        return guestList;
-    }
+  public GuestList getGuestList()
+  {
+    return guestList;
+  }
 
-    public void setGuestList(GuestList guestList){
-        this.guestList = guestList;
-    }
+  public void setGuestList(GuestList guestList)
+  {
+    this.guestList = guestList;
+  }
 
-    public Guest getBooker() {
-        return this.guestList.getMainGuest();
-    }
+  /*public GuestList getBooker()
+  {
+    return GuestList.getMainGuest();
+  }*/
+  public double getPrice()
+  {
+    return price;
+  }
 
-    public double getPrice() {
-        return price;
-    }
+  public void applyDiscount(int discount)
+  {
+    this.price = (this.price * discount) / 100;
+  }
 
-    public void applyDiscount(int discount) {
-        this.price = (this.price * discount) / 100;
-    }
+  public int getNumberExpectedGuests()
+  {
+    return numberExpectedGuests;
+  }
 
-    public int getNumberExpectedGuests() {
-        return numberExpectedGuests;
-    }
+  public void setNumberExpectedGuests(int numeber)
+  {
+    this.numberExpectedGuests = numeber;
+  }
 
-    public void setNumberExpectedGuests(int number) {
-        this.numberExpectedGuests = number;
-    }
+  public double totalPrice()
+  {
+    return price * dateInterval.calcP(getDateInterval().getCheckInDate(),
+        getDateInterval().getCheckOutDate());
+  }
 
-
-    @Override
-    public String toString() {
-        return "Reservation{" + "roomList=" + roomList + ", dateInterval="
-                + dateInterval + ", guestList=" + guestList + ", price=" + price
-                + ", numberExpectedGuests=" + numberExpectedGuests + ", extraBed="
-                + extraBed + '}';
-    }
+   public String toString()
+  {
+    return "Reservation{" + "roomlist=" + roomlist + ", dateInterval="
+        + dateInterval + ", guestList=" + guestList + ", price=" + price
+        + ", numberExpectedGuests=" + numberExpectedGuests + ", extraBed="
+        + extraBed + '}';
+  }
 }
+
+
