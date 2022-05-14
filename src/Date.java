41776b9efb8515b2b14d52fbf4eaef18cc55cd40
AmeyaMahankal <ameya.mahankal@gmail.com>
2022-05-14 16:11:51 +0200

    Updated Date with efficient methods
    new isLeap(),nextDay(),daysInMonth()
    
    Co-Authored-By: BozhiInTheHouse <95079544+BozhiInTheHouse@users.noreply.github.com>

--- src/Date.java
+++ src/Date.java
@@ -34,116 +34,40 @@ public class Date {
         this.year = year;
     }
 
-    public boolean isLeapYear()
-    {
-        if(year%4==0 && year%400==0 || year%4==0 && year%100!=0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
+    public boolean isLeap() {
+        boolean isLeapYear = false;
+        int year = getYear();
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 400 == 0))
+            isLeapYear = true;
+        return isLeapYear;
+    }
+
+    public void nextDay() {
+        day++;
+        if (day > daysInMonth()) {
+            day = 1;
+            month++;
+            if (month > 12) {
+                month = 1;
+                year++;
+            }
         }
     }
 
-    public void nextDay()
-    {
-
-        day=day+1;
-        switch(month)
-        {
-            case 1:
-                if (day==32);
-            {
-                day=1;
-                month=month+1;
+    public int daysInMonth() {
+        int days;
+        if (month == 2) {
+            if (isLeap()) {
+                days = 29;
+            } else {
+                days = 28;
             }
-            case 3:
-                if (day==32);
-            {
-                day=1;
-                month=month+1;
-            }
-            case 5:
-                if (day==32);
-            {
-                day=1;
-                month=month+1;
-            }
-            case 7:
-                if (day==32);
-            {
-                day=1;
-                month=month+1;
-            }
-            case 8:
-                if (day==32);
-            {
-                day=1;
-                month=month+1;
-            }
-            case 10:
-                if (day==32);
-            {
-                day=1;
-                month=month+1;
-            }
-            case 12:
-                if (day==32);
-            {
-                day=1;
-                month=month+1;
-            }
-            case 4:
-                if (day==31)
-                {
-                    day = 1;
-                    month = month + 1;
-                }
-            case 6:
-                if (day==31)
-                {
-                    day = 1;
-                    month = month + 1;
-                }
-            case 9:
-                if (day==31)
-                {
-                    day = 1;
-                    month = month + 1;
-                }
-            case 11:
-                if (day==31)
-                {
-                    day = 1;
-                    month = month + 1;
-                }
-            case 2:
-
-                if(isLeapYear() ) /* ( (Year%4==0) && (Year%100!=0) ) || ((Year%4==0) && (Year%400==0))*/
-                {
-                    if (day==30)
-                    {
-                        day=1;
-                        month=month+1;
-                    }
-                }
-                else
-                {
-                    if (day == 29)
-                    {
-                        day = 1;
-                        month = month + 1;
-                    }
-                }
+        } else if ((month < 8 && month % 2 == 1) || (month > 7 && month % 2 == 0)) {
+            days = 31;
+        } else {
+            days = 30;
         }
-        if (month==13)
-        {
-            year=year+1;
-            month=1;
-            day=1;
-        }
-
+        return days;
     }
 
     public Date copy() {
