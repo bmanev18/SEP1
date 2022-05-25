509b5816f662911ddab34a71ebd5e9c00dc37f1f
radoslavKiryazov <radokiryazov11230@gmail.com>
2022-05-25 20:21:02 +0200

    Assigned Controller to OpeningScene.fxml
    -The two buttons now work

--- GUI/Controller.java
+++ GUI/Controller.java
@@ -1,7 +1,37 @@
 package GUI;
-import javafx.event.ActionEvent;
 
-public class Controller {
-    public void handleClickMe(ActionEvent e)
-    { System.out.println("Hello"); }
+import java.io.IOException;
+import java.util.Objects;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Controller
+{
+
+  private Stage stage;
+  private Scene scene;
+  private Parent root;
+
+  public void switchToManagingRooms(ActionEvent event) throws IOException
+  {
+    root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ManagingRooms.fxml")));
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public void switchToReservations(ActionEvent event) throws IOException
+  {
+    Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Reservations.fxml")));
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
 }
